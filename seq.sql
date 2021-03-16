ebf260d2e52e3ae17ea2100e3c5a2cc0abf086d7
hunbby <rn1349@naver.com>
2021-03-16 15:04:09 +0900

    데이터베이스 테이블, 시퀀스 설정

---
+++ seq.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-3월-16-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence WK_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "KIMI_WORK"."WK_SEQ"  MINVALUE 1 MAXVALUE 99999 INCREMENT BY 1 START WITH 10 NOCACHE  NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence NOTICE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "KIMI_WORK"."NOTICE_SEQ"  MINVALUE 1 MAXVALUE 99999 INCREMENT BY 1 START WITH 6 NOCACHE  NOORDER  NOCYCLE ;
